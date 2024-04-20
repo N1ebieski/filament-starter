a827825bd07eaa449f7a46340b6be05d0737b5e1
N1ebieski <kontakt@intelekt.net.pl>
2024-04-20 11:31:39 +0000

    pipeline implementation

--- app/Overrides/Illuminate/Pipeline/Pipeline.php
+++ app/Overrides/Illuminate/Pipeline/Pipeline.php
@@ -4,16 +4,17 @@ declare(strict_types=1);
 
 namespace App\Overrides\Illuminate\Pipeline;
 
+use Override;
 use Illuminate\Pipeline\Pipeline as BasePipeline;
-use App\Overrides\Illuminate\Contracts\Chain\Chain as ChainContract;
 use App\Overrides\Illuminate\Contracts\Pipeline\Pipeline as PipelineContract;
 
-final class Pipeline extends BasePipeline implements PipelineContract, ChainContract
+final class Pipeline extends BasePipeline implements PipelineContract
 {
     /**
      * @param mixed $traveler
      * @return mixed
      */
+    #[Override]
     public function process(mixed $traveler)
     {
         return $this->send($traveler)->thenReturn();
