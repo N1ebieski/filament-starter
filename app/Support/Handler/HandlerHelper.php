84eafb192bbdebcf87c479204f9ecc33361a83e2
N1ebieski <kontakt@intelekt.net.pl>
2024-06-01 11:20:33 +0000

    refactoring

--- app/Support/Handler/HandlerHelper.php
+++ app/Support/Handler/HandlerHelper.php
@@ -6,13 +6,18 @@ namespace App\Support\Handler;
 
 use ReflectionClass;
 use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
 use App\Support\Attributes\Handler;
 
 final class HandlerHelper
 {
-    public function __construct(private readonly Str $str)
-    {
-    }
+    /** @property array */
+    private const NAMES = [
+        'Query',
+        'CacheQuery',
+        'Command',
+        'Client'
+    ];
 
     /**
      * Finds and returns the Handler namespace for the specified object.
@@ -21,7 +26,7 @@ final class HandlerHelper
      *
      * If not, it looks for a Handler with an identical namespace.
      */
-    public function getNamespace(object $class): string
+    public static function getNamespace(object $class): string
     {
         /** @var string */
         $classNamespace = get_class($class);
@@ -39,15 +44,21 @@ final class HandlerHelper
 
         $classBasename = class_basename($class);
 
-        $handlerNamespace = $this->str->beforeLast($classNamespace, '\\');
+        $handlerNamespace = Str::beforeLast($classNamespace, '\\');
 
-        $lastOccurance = $this->str->match('/([A-Z][^A-Z]*)$/', $classBasename);
+        $names = Collection::make(self::NAMES)
+            ->map(function (string $name): string {
+                return preg_quote($name);
+            })
+            ->implode('|');
+
+        $lastOccurance = Str::match('/(' . $names . ')$/', $classBasename);
 
         if (empty($lastOccurance)) {
             throw new IncorrectNameException("The class name: \"{$classBasename}\" is incorrect.");
         }
 
-        $handlerName = $this->str->replaceLast($lastOccurance, 'Handler', $classBasename);
+        $handlerName = Str::replaceLast($lastOccurance, 'Handler', $classBasename);
 
         $handlerNamespace = $handlerNamespace . '\\' . $handlerName;
 
