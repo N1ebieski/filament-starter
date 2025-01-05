29f6e75c205687234a38427af4eea919a57fdc37
N1ebieski <kontakt@intelekt.net.pl>
2025-01-05 16:38:57 +0000

    refactoring

--- resources/views/components/topbar/offline-state/offline-state-component.blade.php
+++ resources/views/components/topbar/offline-state/offline-state-component.blade.php
@@ -1,15 +1,15 @@
 <div class="flex items-center">
     <div>
-        <div 
-            x-data="{ show: !navigator.onLine }" 
+        <div
+            x-data="{ show: !navigator.onLine }"
             x-init="
                 window.addEventListener('online', () => show = false);
                 window.addEventListener('offline', () => show = true);
-            "        
+            "
             x-tooltip="{
                 content: '{{ trans('offline.offline_mode') }}',
                 theme: $store.theme,
-            }"        
+            }"
         >
             <x-filament::icon
                 icon="hugeicons-cellular-network-offline"
