268fbfa46f2c71f0859972d812577465ab56fa31
N1ebieski <kontakt@intelekt.net.pl>
2025-04-03 07:37:37 +0000

    rector config

--- rector.php
+++ rector.php
@@ -3,10 +3,6 @@
 declare(strict_types=1);
 
 return \Rector\Config\RectorConfig::configure()
-    ->withRules([
+    ->withSets([
         \RectorLaravel\Set\LaravelLevelSetList::UP_TO_LARAVEL_110,
-    ])
-    ->withPreparedSets(
-        deadCode: true,
-        codeQuality: true
-    );
+    ]);
