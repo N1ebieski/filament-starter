1af8fc7af1079472f39b49c1da8d7360c64e16f2
N1ebieski <kontakt@intelekt.net.pl>
2025-04-03 19:29:33 +0000

    refactoring

--- app/Providers/Filament/AdminPanel/AdminPanelServiceProvider.php
+++ app/Providers/Filament/AdminPanel/AdminPanelServiceProvider.php
@@ -26,7 +26,7 @@ final class AdminPanelServiceProvider extends PanelServiceProvider
             ->id(self::ID)
             ->path(self::ID)
             ->homeUrl('/'.self::ID)
-            ->brandName(Lang::string('admin.pages.panel.title')) // @phpstan-ignore-line
+            ->brandName(Lang::string('admin.pages.panel.title'))
             ->discoverResources(in: app_path('Filament/Resources/Admin'), for: 'App\\Filament\\Resources\\Admin')
             ->discoverPages(in: app_path('Filament/Pages/Admin'), for: 'App\\Filament\\Pages\\Admin')
             ->pages([])
