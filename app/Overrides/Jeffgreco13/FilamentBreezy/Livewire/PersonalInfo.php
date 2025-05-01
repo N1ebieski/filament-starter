f1fc74d6c500296e0caa0ea5f1ef9563a227821c
N1ebieski <kontakt@intelekt.net.pl>
2025-05-01 10:44:01 +0000

    increase columnspan

--- app/Overrides/Jeffgreco13/FilamentBreezy/Livewire/PersonalInfo.php
+++ app/Overrides/Jeffgreco13/FilamentBreezy/Livewire/PersonalInfo.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace App\Overrides\Jeffgreco13\FilamentBreezy\Livewire;
 
 use App\Models\User\User;
+use App\Overrides\Jeffgreco13\FilamentBreezy\BreezyCore;
+use Filament\Facades\Filament;
+use Filament\Forms\Components\Group;
 use Jeffgreco13\FilamentBreezy\Livewire\PersonalInfo as BasePersonalInfo;
 use Override;
 
@@ -23,5 +26,20 @@ final class PersonalInfo extends BasePersonalInfo
             'name' => $this->user->name->value,
             'email' => $this->user->email->value,
         ]);
+    }
+
+    protected function getProfileFormSchema(): array
+    {
+        $groupFields = Group::make([
+            $this->getNameComponent(),
+            $this->getEmailComponent(),
+        ])->columnSpan($this->hasAvatars ? 2 : 'full');
+
+        /** @var BreezyCore $breezyCore */
+        $breezyCore = Filament::getPlugin('filament-breezy');
+
+        return ($this->hasAvatars)
+            ? [$breezyCore->getAvatarUploadComponent(), $groupFields]
+            : [$groupFields];
     }
 }
